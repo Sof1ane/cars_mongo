b9e8ae5fa4a76009f9fd2f042f1f49e150ad8cd1
Sofiane <sofianeomari.so@gmail.com>
2021-12-10 11:06:51 +0100

    update

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+# streamlit_app.py
+
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+client = pymongo.MongoClient(**st.secrets["mongo"])
+
+# Pull data from the collection.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+
